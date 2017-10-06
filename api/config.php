70878c3fd8390dbb3aacb758f5ccf121ccaceb89
Tarley <tarley.lana@gmail.com>
2017-10-06 23:28:43 +0000

    Incluindo o Log Entries

--- api/config.php
+++ api/config.php
@@ -15,9 +15,9 @@ define('ENV', 'Dev');
 /* CONFIGURAÇÕES LOGENTRIES */
 $LOGENTRIES_TOKEN = "0fff9a74-e2b2-40d7-965e-215f0c01e8c5";
 
-require_once ABSPATH . '/api/util/log/logentries.php';
-require_once ABSPATH . '/api/util/log/log.php';
+require_once 'util/log/logentries.php';
+require_once 'util/log/log.php';
 
 Log::$log = $log;
 
-Log::Debug("Teste de log");
+//Log::Debug("Teste de log");
